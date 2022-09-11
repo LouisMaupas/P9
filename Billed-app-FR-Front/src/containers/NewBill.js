6026a6452c5f7ee460d1d98f59d4fcd435e93e5b
lmaupas <louis.maupas@framatome.com>
2022-09-11 09:22:21 +0200

    fix [Bug Hunt] - Bills

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -21,7 +21,6 @@ export default class NewBill {
     e.preventDefault();
     const file = this.document.querySelector(`input[data-testid="file"]`)
       .files[0];
-    console.log(file);
     const filePath = e.target.value.split(/\\/g);
     const fileName = filePath[filePath.length - 1];
     const formData = new FormData();
